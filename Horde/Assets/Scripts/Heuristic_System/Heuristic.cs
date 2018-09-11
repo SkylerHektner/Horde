6deed1fa98c21ee2bf2d62e1a0da28aeb2d11cb2
Skyler Hektner <skylerhektner@gmail.com>
2018-09-11 14:57:27 -0600

    Added in base for Heuristic system

---
+++ Horde/Assets/Scripts/Heuristic_System/Heuristic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// I am the base class for all Heuristics. Inheret from me when making a heuristic. Make
+/// sure you override Init, Update, and Resolve.
+/// </summary>
+public class Heuristic : MonoBehaviour {
+
+    public virtual void Init()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Resolve()
+    {
+
+    }
+	
+}
---
+++ Horde/Assets/Scripts/Heuristic_System/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the base class for all units in the game. It is responsible for managing Heuristics.
+/// It also exposes information that Heuristics might be interested in like health, current target, etc...
+/// </summary>
+public class Unit : MonoBehaviour
+{
+    [SerializeField]
+    private bool useHeuristicSwapping = true;
+
+    private List<Heuristic> behaviors = new List<Heuristic>();
+
+    public int CurrentHealth;
+
+    public Unit CurrentTarget;
+}
