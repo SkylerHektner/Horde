ca35975860d49980f0e96d446bd6f4bc2b12815f
Skyler Hektner <skylerhektner@gmail.com>
2018-09-12 10:51:44 -0600

    quick fix on compile error

--- Horde/Assets/Scripts/Heuristic_System/TestHeuristic.cs
+++ Horde/Assets/Scripts/Heuristic_System/TestHeuristic.cs
@@ -1,7 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestHeuristic : Heuristic {
+public class TestHeuristic : Heuristic
+{
+    public override void Execute()
+    {
+        throw new NotImplementedException();
+    }
 
+    public override void Init()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Resolve()
+    {
+        throw new NotImplementedException();
+    }
 }
