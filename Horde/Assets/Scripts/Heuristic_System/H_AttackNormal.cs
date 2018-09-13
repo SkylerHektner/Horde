cb3b43c27e54c3908691d8487b65cdd3dfc93e9c
mcdonaldlevi <mcdonaldlevi@ymail.com>
2018-09-12 19:24:39 -0600

    Added attack

---
+++ Horde/Assets/Scripts/Heuristic_System/H_AttackNormal.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// --Seek Heuristic--
+/// 
+/// Walks straight forward until it see's an enemy.
+/// Movement stops upon seeing enemy.
+/// </summary>
+public class H_AttackNormal : Heuristic
+{
+    [SerializeField]
+    private float attackSpeed = 1.0f;
+
+    [SerializeField]
+    private float attackRange = 3f;
+
+    private Unit enemy;
+
+    [SerializeField]
+    private float attackTime;
+
+    private bool attackInProgress;
+
+    public override void Init() // Initializing the behavior.
+    {
+        base.Init();
+        enemy = unit.CurrentTarget;
+    }
+
+    public override void Execute() // Logic that should be called every tick.
+    {
+        if (!attackInProgress)
+        {
+            StartCoroutine(AttackAnim());
+            enemy.CurrentHealth -= 1;
+        }
+    }
+
+    public override void Resolve() // Exiting the behavior.
+    {
+        //Set code for after target dies
+
+        base.Resolve();
+    }
+
+    IEnumerator AttackAnim()
+    {
+        attackInProgress = true;
+        var initialPosition = transform.position;
+        transform.position = enemy.transform.position;
+        yield return new WaitForSeconds(attackTime);
+        transform.position = initialPosition;
+        yield return new WaitForSeconds(attackSpeed);
+        attackInProgress = false;
+
+    }
+}
---
+++ Horde/Assets/Scripts/Heuristic_System/HueristicInterface1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HInterface : MonoBehaviour {
+
+	public enum HType
+    {
+        Seek, NormalAttack
+    }
+
+    public static System.Type GetHeuristic(HType heuristic)
+    {
+        switch(heuristic)
+        {
+            case (HType.Seek):
+                return typeof(H_Seek);
+            case (HType.NormalAttack):
+                return typeof(H_AttackNormal);
+        }
+
+
+
+        return null;
+    }
+}
