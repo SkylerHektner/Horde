182d82b8c5124ca925adeda664be86091cc35366
Joey Weidman <pcjoe3343@gmail.com>
2018-09-14 17:30:26 -0600

    Made whitebox level, got the navmesh working on it

---
+++ Horde/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
