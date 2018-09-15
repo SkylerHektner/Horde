347e1483c936cefd74c04c5ecfd88eea44b94b06
Joey Weidman <pcjoe3343@gmail.com>
2018-09-15 10:34:37 -0600

    Added wander AI

---
+++ Horde/Assets/Scripts/Wander.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wander : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        InvokeRepeating("SetNewWanderPosition", 0f, 7f);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        // If the unit is stuck on a wall or something
+        if(agent.velocity == Vector3.zero)
+        {
+            SetNewWanderPosition();
+        }
+    }
+
+    private Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+        randDirection += origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+
+    private void SetNewWanderPosition()
+    {
+        Vector3 newPos = RandomNavSphere(transform.position, 50, -1);
+        agent.SetDestination(newPos);
+    }
+}
