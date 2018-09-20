3f3a8fcc2032be51cd2a3e6e7f15fd69c3831b46
Joey Weidman <pcjoe3343@gmail.com>
2018-09-20 01:43:50 -0600

    Added blank enemy attack script to update enemy prefab

---
+++ Horde/Assets/Scripts/TempAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// TEMPORARY ATTACK CLASS
+/// FOR TESTINg PURPOSES
+/// </summary>
+public class TempAttack : MonoBehaviour
+{
+    private float attackRange = 3;
+    private float attackVelocity = 15;
+
+	void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+        
+	}
+}
