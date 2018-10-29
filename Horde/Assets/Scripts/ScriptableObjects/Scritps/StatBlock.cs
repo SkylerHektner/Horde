3363edf6309801e73ac5f084155df6cc089c451a
Joey Weidman <pcjoe3343@gmail.com>
2018-10-29 15:10:04 -0600

    Added stat block scriptable object

---
+++ Horde/Assets/Scripts/ScriptableObjects/Scritps/StatBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// The container for all of the shared data for a base unit.
+///
+/// </summary>
+[CreateAssetMenu(fileName = "StatBlock", menuName = "StatBlock")]
+public class StatBlock : ScriptableObject
+{
+	[SerializeField]
+	private int maxHealth;
+	public int MaxHealth { get { return maxHealth; } }
+
+	[SerializeField]
+	private float movementSpeed;
+	public float MovementSpeed { get { return movementSpeed; } }
+
+	[SerializeField]
+	private string unitType;
+	public string UnitType { get { return unitType ; } }
+}
