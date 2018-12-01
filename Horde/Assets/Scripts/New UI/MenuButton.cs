1f183ba0e5df7289577fcc58fd8ed55ad9e62099
Joey Weidman <pcjoe3343@gmail.com>
2018-12-01 01:26:58 -0700

    Added foundation for radial menu

---
+++ Horde/Assets/Scripts/New UI/MenuButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+  
+[RequireComponent (typeof (RectTransform), typeof (Collider2D))]
+public class MenuButton : MonoBehaviour, ICanvasRaycastFilter, IPointerEnterHandler
+{
+    [SerializeField]
+    private HInterface.HType heuristic;
+
+    [SerializeField]
+    private RadialMenu radialMenu;
+
+    [SerializeField]
+    private Sprite icon;
+
+    private Collider2D myCollider;
+    private RectTransform rectTransform;
+
+    private Color buttonColor;
+    
+    void Awake () 
+    {
+        myCollider = GetComponent<Collider2D>();
+        rectTransform = GetComponent<RectTransform>();
+
+        buttonColor = GetComponent<Image>().color;
+    }
+
+    // So the button is only detected when the mouse hovers over the image.
+    public bool IsRaycastLocationValid (Vector2 screenPos, Camera eventCamera)
+    {
+     	var worldPoint = Vector3.zero;
+        var isInside = RectTransformUtility.ScreenPointToWorldPointInRectangle(
+         	rectTransform,
+         	screenPos,
+         	eventCamera,
+         	out worldPoint
+        );
+
+        if (isInside)
+          isInside = myCollider.OverlapPoint(worldPoint);
+
+        return isInside;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        radialMenu.SetCenterIcon(icon);
+        radialMenu.SetCenterName(heuristic.ToString());
+        radialMenu.SetCenterColor(buttonColor);
+    }
+}
+ 
---
+++ Horde/Assets/Scripts/New UI/RadialMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialMenu : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject centerDisplay;
+
+	[SerializeField]
+	private Image centerDisplayIcon;
+
+	[SerializeField]
+	private Text centerHeuristicName;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	public void SetCenterIcon(Sprite s)
+	{
+		centerDisplayIcon.sprite = s;
+	}
+
+	public void SetCenterName(string s)
+	{
+		centerHeuristicName.text = s;
+	}
+
+	public void SetCenterColor(Color c)
+	{
+		centerDisplay.GetComponent<Image>().color = c;
+	}
+}
