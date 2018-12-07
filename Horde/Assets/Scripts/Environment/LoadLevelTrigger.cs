d35feb7e0f612ed29609ca35416d2c4ae49c53b2
Skyler Hektner <skylerhektner@gmail.com>
2018-12-06 17:25:12 -0700

    Added level move to next level functionallity

---
+++ Horde/Assets/Scripts/Environment/LoadLevelTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelTrigger : MonoBehaviour
+{
+    public string nextScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (nextScene != null && other.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
