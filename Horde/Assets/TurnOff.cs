6dee0e3a922d610da6b5e2fbe5ae3bc20c72c5aa
Amedes95 <pielord95@gmail.com>
2019-01-12 22:47:34 -0700

    Togglable flashlight added
    Who know's when this will be useful

---
+++ Horde/Assets/TurnOff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOff : MonoBehaviour
+{
+    public Light headLamp;
+    public AudioSource flashlight;
+    public AudioClip ClickOn;
+    public AudioClip ClickOff;
+
+    private void Awake()
+    {
+
+        headLamp = GetComponent<Light>();
+        flashlight = gameObject.GetComponent<AudioSource>();
+
+
+    }
+
+    private void Update()
+    {
+
+        if (headLamp != null && Input.GetKeyUp(KeyCode.L))
+        {
+            headLamp.enabled = !headLamp.enabled;
+            if (headLamp.enabled == true)
+            {
+                flashlight.clip = ClickOff;
+            }
+            if (headLamp.enabled == false)
+            {
+                flashlight.clip = ClickOn;
+            }
+            flashlight.Play();
+
+        }
+
+    }
+}
