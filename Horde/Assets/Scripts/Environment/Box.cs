59680aae11ba5cc044d44422f215c085bac0574d
Joey Weidman <pcjoe3343@gmail.com>
2019-01-18 21:04:26 -0700

    Boxes toggle kinematic properly now

--- Horde/Assets/Scripts/Environment/Box.cs
+++ Horde/Assets/Scripts/Environment/Box.cs
@@ -5,6 +5,30 @@ using UnityEngine;
 public class Box : MonoBehaviour, IBreakable
 {
 	public GameObject brokenVersion;
+	
+	private float distToGround;
+	private LayerMask mask;
+	private LayerMask rayCastPlane;
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		distToGround = GetComponentInChildren<Collider>().bounds.extents.y;
+
+		mask = 1 << LayerMask.NameToLayer("Ground");
+		rayCastPlane = 1 << LayerMask.NameToLayer("RaycastPlane");
+		mask = mask | ~rayCastPlane;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		if(IsFloating())
+			rb.isKinematic = false;
+		else
+			StartCoroutine(ChangeToKinematic()); // We need to use a coroutine to give it an extra 1/10 of a second
+												 // before we set kinematic back to true or else it sinks into the ground.
+	}
 
 	public void Break()
 	{
@@ -20,5 +44,29 @@ public class Box : MonoBehaviour, IBreakable
 	public Vector3 GetPosition()
 	{
 		return transform.position;
+	}
+
+	private bool IsFloating()
+	{
+		RaycastHit hit;
+		Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity, mask);
+		Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.red);
+
+		//Debug.Log(hit.distance);
+		//Debug.Log(distToGround);
+
+		Debug.Log(hit.distance - distToGround);
+
+		if(hit.distance - distToGround > 0.1f)
+			return true;
+		else
+			return false;
+	}
+
+	private IEnumerator ChangeToKinematic()
+	{
+		yield return new WaitForSeconds(0.1f);
+
+		rb.isKinematic = true;
 	}
 }
