99ec9d675f3002edce6a37208e07f2d930c09669
Skyler Hektner <skylerhektner@gmail.com>
2019-01-22 08:52:37 -0700

    Fixed small bug with pressure plates. Can now also toggle whether they can be activated by gaurds in the editor

--- Horde/Assets/Scripts/Environment/PressurePlate.cs
+++ Horde/Assets/Scripts/Environment/PressurePlate.cs
@@ -6,36 +6,39 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour {
 
     public GameObject[] listeners;
+    public bool guardsCanTrigger = false;
+
+    private int onCount = 0;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" || guardsCanTrigger)
         {
-            if (listeners != null)
+            onCount++;
+            if (listeners != null && onCount == 1)
             {
                 foreach (GameObject handler in listeners)
                 {
                     handler.GetComponent<ITriggerHandler>().TriggerOn();
                 }
+                Debug.Log("TRIGGERED");
             }
-            Debug.Log("TRIGGERED");
         }
-
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" || guardsCanTrigger)
         {
-            if (listeners != null)
+            onCount--;
+            if (listeners != null && onCount == 0)
             {
                 foreach (GameObject handler in listeners)
                 {
                     handler.GetComponent<ITriggerHandler>().TriggerOff();
                 }
+                Debug.Log("UN TRIGGERED");
             }
-            Debug.Log("UN TRIGGERED");
         }
-
     }
 }
