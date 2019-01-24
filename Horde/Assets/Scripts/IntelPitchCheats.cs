0c2971b64d101013556bc6f4eed258c1ae2db512
Joey Weidman <u0790967@ad.utah.edu>
2019-01-23 22:39:44 -0700

    Created a script to easily navigate to different scenes

---
+++ Horde/Assets/Scripts/IntelPitchCheats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntelPitchCheats : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown("1"))
+        {
+
+        }
+        else if(Input.GetKeyDown("2"))
+        {
+
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+
+        }
+        else if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
