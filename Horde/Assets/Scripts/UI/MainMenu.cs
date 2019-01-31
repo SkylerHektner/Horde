7f456d03c4e6740797af1eebb0c35f903e2f6079
Amedes95 <pielord95@gmail.com>
2019-01-31 13:48:16 -0700

    New Menu UI

--- Horde/Assets/Scripts/UI/MainMenu.cs
+++ Horde/Assets/Scripts/UI/MainMenu.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour 
 {
-	public void LoadLevel1()
+
+    [SerializeField]
+    public GameObject Menu;
+    public GameObject Settings;
+    public Transform cameraLocationSettings;
+    public Transform cameraLocationMenu;
+
+
+    private void Start()
+    {
+        Settings.SetActive(false);
+    }
+
+    public void LoadLevel1()
 	{
 		SceneManager.LoadScene(1);
 	}
@@ -19,4 +33,21 @@ public class MainMenu : MonoBehaviour
 	{
 		Application.Quit();
 	}
+
+    public void LoadSettings()
+    {
+        Vector3 newPos = new Vector3(cameraLocationSettings.position.x, cameraLocationSettings.position.y - 5f, cameraLocationSettings.position.z + 35f);
+        GameManager.Instance.SetCameraLocation(newPos);
+        Menu.SetActive(false);
+        Settings.SetActive(true);
+
+    }
+
+    public void LeaveSettings()
+    {
+        Vector3 newPos = new Vector3(cameraLocationMenu.position.x, cameraLocationMenu.position.y, cameraLocationMenu.position.z);
+        GameManager.Instance.SetCameraLocation(newPos);
+        Menu.SetActive(true);
+        Settings.SetActive(false);
+    }
 }
