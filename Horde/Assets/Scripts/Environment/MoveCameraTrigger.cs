ee0639fb7f9a8eb48c591bb86097236429008244
Joey Weidman <pcjoe3343@gmail.com>
2019-02-21 23:01:16 -0700

    Fixed guard sizing. Added camera movement tool prefab.

---
+++ Horde/Assets/Scripts/Environment/MoveCameraTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform newTransform;
+
+    private CameraController cc;
+
+    void Start()
+    {
+        cc = FindObjectOfType<CameraController>();
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.tag == "Player")
+            cc.MoveTo(newTransform);
+    }
+}
