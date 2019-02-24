853c78acb06c466402f33ed5bde862f6c3d7b453
Joey Weidman <pcjoe3343@gmail.com>
2019-02-23 18:18:16 -0700

    Made editor tool to choose which room you want to player to start in.

---
+++ Horde/Assets/Scripts/Editor/SetStartingRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+public class SetStartingRoom : ScriptableWizard
+{
+    [SerializeField] private string roomName;
+
+    [MenuItem ("Tools For Alex/SetStartingRoom")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<SetStartingRoom> ("Set Starting Room", "Set");
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject roomGO = GameObject.Find(roomName);
+        Room r = roomGO.GetComponent<Room>();
+        Transform t = r.transform.Find("Player-Spawn");
+
+        Player p = GameObject.FindObjectOfType<Player>();
+        p.GetComponent<NavMeshAgent>().Warp(t.position);
+        p.transform.rotation = t.rotation;
+    }
+}
