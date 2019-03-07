0918d5fd9b280b36494a6b2a5bed07b9a1cce9db
tim <tim.taznar@gmail.com>
2019-03-07 12:29:32 -0700

    SoundEffects Basic script added

---
+++ Horde/Assets/Scripts/Sound/SoundEffects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip[] AudioClips;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayEventSount()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return AudioClips[UnityEngine.Random.Range(0, AudioClips.Length)];
+    }
+
+}
