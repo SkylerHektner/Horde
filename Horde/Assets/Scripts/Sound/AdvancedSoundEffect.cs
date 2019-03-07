86e3d9254387b78bb6b73a57e31b789f6048ad3c
tim <tim.taznar@gmail.com>
2019-03-07 13:00:39 -0700

    Advanced Sound Effect Script

---
+++ Horde/Assets/Scripts/Sound/AdvancedSoundEffect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ClipList
+{
+    public string Name;
+    public List<AudioClip> Clips;
+}
+
+[System.Serializable]
+public class AdvancedSoundEffect : MonoBehaviour
+{
+    public List<ClipList> Clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void PlayEventSound(string name, int clipnumber)
+    {
+        int index = GetNameIndex(name);
+        ClipList cliplist = Clips[index];
+        AudioClip clip;
+        if(clipnumber < 0)
+        {
+            clip = GetRandomClip(index);
+        }
+        else
+        {
+            clip = cliplist.Clips[clipnumber];
+        }
+
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip(int index)
+    {
+        return Clips[index].Clips[Random.Range(0, Clips[index].Clips.Count)];
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, Clips.Count);
+        return Clips[index].Clips[Random.Range(0, Clips[index].Clips.Count)];
+    }
+
+    private int GetNameIndex(string name)
+    {
+        for (int i = 0; i < Clips.Count; i++)
+        {
+            if(Clips[i].Name == name)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
