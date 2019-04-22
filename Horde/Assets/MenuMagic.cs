3455ccb48b867e56682fc70cb4f69aa9cfc9fd72
Amedes95 <pielord95@gmail.com>
2019-04-21 18:20:25 -0600

    Hooked up Main Menu
    Need Joey's help to do the rest of it

---
+++ Horde/Assets/MenuMagic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMagic : MonoBehaviour
+{
+    public GameObject PlayGameCanvas;
+    public GameObject ProgressCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGameCanvas.SetActive(true);
+        ProgressCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void HidePlayGameCanvas()
+    {
+        PlayGameCanvas.SetActive(false);
+        ProgressCanvas.SetActive(true);
+    }
+
+    public void BeginNewGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
