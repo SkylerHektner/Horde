7a8b0be4297da9f25c8f4eada84f98d9d17af43f
Amedes95 <pielord95@gmail.com>
2019-04-22 21:18:18 -0600

    Cutscene Trigger

---
+++ Horde/Assets/Cutscene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+
+    public GameObject FinalScene;
+    GameObject Player;
+    GameObject Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider playa)
+    {
+        Player.SetActive(false);
+        Camera.SetActive(false);
+        FinalScene.SetActive(true);
+    }
+}
